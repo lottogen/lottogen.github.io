bb0060ffacc5ad6dad1c129c2916d8c482f38d22
lottogen <>
2022-06-14 22:08:32 -0600

    Increased temp stats size

--- lottoGen.js
+++ lottoGen.js
@@ -532,7 +532,7 @@ function calc_tempStats(history, tempCount, tempStats, lowLimit, midLimit) {
   var tempZeros = [];
   var tempMid = [];
   var tempRest = [];
-  for (var k = (history.length - 240); k < history.length; k++) {
+  for (var k = (history.length - 280); k < history.length; k++) {
     var seq = history[k].main.split("-");
     if (seq.length === 1) seq = history[k].main.split(" ");
     var bonus = history[k].bonus;
@@ -542,7 +542,7 @@ function calc_tempStats(history, tempCount, tempStats, lowLimit, midLimit) {
       bType = "G: ";
     }
 
-    if (k > (history.length - 201)) {
+    if (k > (history.length - 241)) {
       tempZeros = [];
       tempMid = [];
       tempRest = [];
